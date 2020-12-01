dc0f48eaa2afd36d22619b88c70f8fb4906dab46
hoseongseo <hosung9009@gmail.com>
2020-12-01 19:17:53 +0900

    initial commit with system

---
+++ +System/+Dynamics/LTV.m
@@ -0,0 +1,23 @@
+classdef LTV < System.Base.Dynamics
+    properties (GetAccess = private, SetAccess = private)
+        A
+        D
+    end
+    methods
+        function this = LTV(nx, nu, nw, Amat, Dmat)
+            this = this@System.Base.Dynamics(nx,nu,nw);
+            this.A = Amat;
+            this.D = Dmat;
+        end
+        % overloading dynamics
+        function f_ = f(this, x, u, w, t)
+            f_ = this.A(t) * x + this.D(t) * w;
+        end
+        function dfdx_ = dfdx(this, x, u, w, t)
+            dfdx_ = this.A(t);
+        end
+        function dfdw_ = dfdw(this, x, u, w, t)
+            dfdw_ = this.D(t);
+        end
+    end
+end
